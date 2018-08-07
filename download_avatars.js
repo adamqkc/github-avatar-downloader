c94d83e5802beb3902eec1e888bffc10a1ef7f76
Adam <adamqkc@gmail.com>
2018-08-07 06:35:23 +0000

    parse JSON and log avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -3,17 +3,28 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function (err, res, body) {
-    cb(err, body);
-  });
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
   request(options, function (err, res, body) {
     cb(err, body);
   });
 }
 
+
 getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", JSON.parse(result)[29]);
+  var parsedResult = JSON.parse(result);
+
+  for (var i = 0; i < parsedResult.length; i++) {
+    console.log(parsedResult[i]['avatar_url']);
+  }
+
 });
+
+// https://api.github.com/repos/jquery/jquery/contributors
